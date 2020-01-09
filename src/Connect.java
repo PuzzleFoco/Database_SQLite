205321cdc6e089e43607a96c6cd06dc6e7f65ddb
PuzzleFoco <fabius-engel@live.de>
2020-01-09 21:22:56 +0100

    feature: InsertExpenses

--- src/Connect.java
+++ src/Connect.java
@@ -7,7 +7,7 @@ public class Connect {
     public static void connect() {
         Connection conn = null;
         try{
-            String url = "jdbc:sqlite:C:/Users/engelf2/IdeaProjects/Database/db/chinook.db";
+            String url = "jdbc:sqlite:C:/Users/Frieda.Schulz/IdeaProjects/Database_SQLite/db/ausgabendatenbank.db";
             conn = DriverManager.getConnection(url);
 
             System.out.println("Connection to SQLIte has been established.");
@@ -26,5 +26,7 @@ public class Connect {
 
     public static void main(String[] args){
         connect();
+        new createNewTable();
+
     }
 }
