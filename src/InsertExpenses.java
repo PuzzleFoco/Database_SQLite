b3cc8054c1cc07692792f6d6b5712373a747c281
Frieda.Schulz <friedaschulz@hotmail.de>
2020-01-10 11:11:44 +0100

    Added new functions: CreateNewTable and InsertExpenses

---
+++ src/InsertExpenses.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.sql.*;
+
+public class InsertExpenses {
+
+    private static Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:/Users/Frieda.Schulz/IdeaProjects/Database_SQLite/db/ausgabendatenbank.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void insert(String reason, double amount, int expenseID) {
+        String sql = "INSERT INTO expenses (reason, amount, expenseID) VALUES (?,?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, reason);
+            pstmt.setDouble(2, amount);
+            pstmt.setInt(3, expenseID);
+            pstmt.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            //System.err.println("got an exception");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int createexpenseID(){
+        int existingID = 1;
+        String sql = "SELECT expenseID FROM expenses";
+
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()){ existingID = rs.getInt("expenseID");}
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return existingID+1;
+    }
+
+    public static void askfornewinsert(){
+        InsertExpenses app = new InsertExpenses();
+        String reason = JOptionPane.showInputDialog("What did you purchase?");
+        double amount = Double.parseDouble(JOptionPane.showInputDialog("How much did it cost?"));
+        int expenseID = createexpenseID();
+
+        app.insert(reason, amount, expenseID);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        askfornewinsert();
+
+    }
+
+}
